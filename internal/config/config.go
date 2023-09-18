dfcb95b40eac33e99d978b69a8537320f0525867
Rohit Jaiswal <rohit.jaiswal@gojek.com>
2023-09-18 18:53:25 +0530

    Added  DB integration

--- internal/config/config.go
+++ internal/config/config.go
@@ -25,5 +25,23 @@ func Init() {
 	_ = viper.ReadInConfig()
 	viper.AutomaticEnv()
 
-	App = &Config{}
+	App = &Config{
+		Database: &DatabaseConfig{
+			Postgres: &PostgresConfig{
+				Host:               getStringOrPanic("DB_HOST"),
+				Port:               getIntOrPanic("DB_PORT"),
+				DatabaseName:       getStringOrPanic("DB_NAME"),
+				Driver:             getStringOrPanic("DB_DRIVER"),
+				Username:           getStringOrPanic("DB_USER"),
+				Password:           getStringOrPanic("DB_PASSWORD"),
+				ReadTimeout:        getDurationInMS("DB_READ_TIMEOUT_IN_MS"),
+				WriteTimeout:       getDurationInMS("DB_WRITE_TIMEOUT_IN_MS"),
+				MaxOpenConnections: getIntOrPanic("DB_MAX_OPEN_CONNECTIONS"),
+				MaxIdleConnections: getIntOrPanic("DB_MAX_IDLE_CONNECTIONS"),
+			},
+		},
+		Server: &ServerConfig{
+			Port: getIntOrPanic("PORT"),
+		},
+	}
 }
